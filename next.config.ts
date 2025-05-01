19a738e4e3f2b316f1125063bac2e3416f512b9f
Oyo Ramos <seph.ramos@icloud.com>
2025-05-01 13:10:55 +0800

    experimenting aasa 25

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,7 @@ const nextConfig: NextConfig = {
             key: 'Content-Type',
             value: 'application/json',
           },
+          { key: 'Cache-Control', value: 'public, max-age=86400' },
         ],
       },
     ];
